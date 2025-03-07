d9b7505175ac38b7709cb1334a5c6171d4e8d184
Daniel C. Silva <danielcs80@gmail.com>
2025-03-07 08:51:04 -0300

    Tipos de dados

--- aula01/tipoDados.js
+++ aula01/tipoDados.js
@@ -1,1 +1,67 @@
-console.log("tipos de dados")
+console.log("Tipos de dados em Javascript");
+// Tipos de dados em Javascript
+// Number
+console.log("Number");
+let numero = 10;
+console.log(numero);
+console.log(typeof numero);
+// String
+console.log("String");
+let texto = "Texto";
+console.log(texto);
+console.log(typeof texto);
+// Boolean
+console.log("Boolean");
+let booleano = true;
+console.log(booleano);
+console.log(typeof booleano);
+// Array
+console.log("Array");
+let array = [1, 2, 3];
+console.log(array);
+console.log(typeof array);
+// Object
+console.log("Object");
+let objeto = {nome: "João", idade: 25};
+console.log(objeto);
+console.log(typeof objeto);
+// Undefined
+console.log("Undefined");
+let indefinido; // undefined
+console.log(indefinido);
+console.log(typeof indefinido);
+// Null
+console.log("Null");
+let nulo = null;
+console.log(nulo);
+console.log(typeof nulo);
+// Function
+console.log("Function");
+let funcao = function() {};
+console.log(funcao);
+console.log(typeof funcao);
+// Date
+console.log("Date");
+let data = new Date();
+console.log(data);
+console.log(typeof data);
+// RegExp
+console.log("RegExp");
+let regex = /teste/;
+console.log(regex);
+console.log(typeof regex);
+// Error
+console.log("Error");
+let erro = new Error();
+console.log(erro);
+console.log(typeof erro);
+// Symbol
+console.log("Symbol");
+let simbolo = Symbol();
+console.log(simbolo);
+console.log(typeof simbolo);
+// BigInt
+console.log("BigInt");
+let bigInt = 10n;
+console.log(bigInt);
+console.log(typeof bigInt);
